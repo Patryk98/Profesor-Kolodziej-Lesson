8374765abe5081e96e82114e319595e7961f4046
Patryk98 <rechniopatryk@gmail.com>
2017-01-09 12:51:46 +0100

    Create form.php

---
+++ form.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Formularz</title>
+</head>
+<body>
+	<form action="form.html" method="post">
+		<p>Imię
+			<input type="text" name="imie" />
+		</p>
+		<p>Nazwisko
+			<input type="text" name="nazwisko" />
+		</p>
+		<p>Adres
+			<input type="text" name="adres" />
+		</p>
+		
+		<select name="produkty" multiple="multiple" size="6">
+			<optgroup label="Grupa 1">
+				<option value="1" selected="selected">Produkt1</option>
+				<option value="2" selected="selected">Produkt2</option>
+			</optgroup>
+			<optgroup label="Grupa 2">
+				<option value="1" selected="selected">Produkt1</option>
+				<option value="2" selected="selected">Produkt2</option>
+			</optgroup>
+		</select>
+		<p>Data Wysyłki
+			<input name="data" type="week" />
+		</p>
+		<p>Wybierz kolor
+			<input name="color" type="color" />
+		</p>
+		<input value="submit" type="submit" />
+	</form>
+</body>
+</html>
