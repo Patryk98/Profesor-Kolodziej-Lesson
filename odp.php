079cf0d52fe3a139d0a8e14312f0ce3860583c99
Patryk98 <rechniopatryk@gmail.com>
2017-01-09 13:05:42 +0100

    Create odp.php

---
+++ odp.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+	<title>Formularz</title>
+</head>
+<body>
+  <?php
+    $imie=$_POST("imie");
+    $nazwisko=$_POST("nazwisko");
+    $adres=$_POST("adres");
+    $color=$_POST("color";
+    $data=$_POST("data");
+    
+    echo $imie;
+    echo " ";
+    echo $nazwisko;
+    echo " ";
+    
+  ?>
+</body
+</html>
